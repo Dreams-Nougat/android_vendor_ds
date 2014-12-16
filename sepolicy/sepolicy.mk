7faf94c2b9e0dcd3824d7e5f3d5398d5eda1b5f0
Carlos Manuel Leemet <carlosmanuel_leemet@msn.com>
2014-12-16 21:24:52 +0200

    CM sepolicy stuff

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -7,6 +7,23 @@ BOARD_SEPOLICY_DIRS += \
     vendor/cm/sepolicy
 
 BOARD_SEPOLICY_UNION += \
-    file_contexts \
+    file.te \
+    genfs_contexts \
+    property_contexts \
     seapp_contexts \
+    service_contexts \
+    auditd.te \
+    adbd.te \
+    healthd.te \
+    hostapd.te \
+    installd.te \
+    netd.te \
+    property.te \
+    recovery.te \
+    shell.te \
+    su.te \
+    sysinit.te \
+    system.te \
+    ueventd.te \
+    vold.te \
     mac_permissions.xml

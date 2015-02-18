09c21731ad5a6c0c1c079af51a4037bcdc97aa40
Carlos Manuel Leemet <carlosmanuel_leemet@msn.com>
2015-02-18 17:42:09 +0200

    Update sepolicy.mk

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -10,18 +10,22 @@ BOARD_SEPOLICY_UNION += \
     file.te \
     file_contexts \
     genfs_contexts \
+    dumpstate.te \
     hostapd.te \
     installd.te \
     netd.te \
     property.te \
     property_contexts \
     recovery.te \
+    servicemanager.te \
     shell.te \
     su.te \
     system.te \
     sysinit.te \
     system_app.te \
+    system_server.te \
     ueventd.te \
+    untrusted_app.te \
     userinit.te \
     vold.te \
     mac_permissions.xml

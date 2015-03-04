135fc062d4e94038c55b847ed3e42e29fce34d48
Carlos Manuel Leemet <carlosmanuel_leemet@msn.com>
2015-03-04 23:41:32 +0200

    Override old AOSP defaults - ringtone, alert, notification sounds

--- configs/aosp_fixes.mk
+++ configs/aosp_fixes.mk
@@ -23,6 +23,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.setupwizard.enterprise_mode=1 \
     ro.com.android.dateformat=MM-dd-yyyy
 
+# Override old AOSP defaults
++PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.ringtone=Ganymede.ogg \
+    ro.config.notification_sound=Tethys.ogg \
+    ro.config.alarm_alert=Osmium.ogg
+
 # Enable SIP+VoIP
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml

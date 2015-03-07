8fee15cc52a04e78950f0603e857c1a8c955d3d9
Carlos Manuel Leemet <carlosmanuel_leemet@msn.com>
2015-03-07 02:59:49 +0200

    Update sepolicy.mk

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -9,11 +9,15 @@ BOARD_SEPOLICY_DIRS += \
 BOARD_SEPOLICY_UNION += \
     file.te \
     file_contexts \
+    dumpstate.te \
     hostapd.te \
+    init.te \
     healthd.te \
     netd.te \
     property.te \
+    sdcardd.te \
     system.te \
+    shell.te \
     sysinit.te \
     system_app.te \
     ueventd.te \

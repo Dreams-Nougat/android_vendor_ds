f7f17fe5802a44b0651980a0fcaf8569e7814aee
Carlos Manuel Leemet <carlosmanuel_leemet@msn.com>
2015-03-07 02:42:55 +0200

    system.mk: show us SELinux status in the Settings

--- configs/system.mk
+++ configs/system.mk
@@ -49,3 +49,6 @@ PRODUCT_COPY_FILES += \
     vendor/$(VENDOR)/prebuilt/bin/backuptool.sh:system/bin/backuptool.sh \
     vendor/$(VENDOR)/prebuilt/bin/backuptool.functions:system/bin/backuptool.functions \
     vendor/$(VENDOR)/prebuilt/bin/50-backupScript.sh:system/addon.d/50-backupScript.sh
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.selinux=1

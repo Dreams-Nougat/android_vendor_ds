63b45b5098e29df7f79a4c026f5321e27f4bcdde
Carlos Manuel Leemet <carlosmanuel_leemet@msn.com>
2015-10-19 14:25:05 +0300

    cleanup sepolicy...

--- sepolicy/old/sepolicy.mk
+++ sepolicy/old/sepolicy.mk
@@ -7,9 +7,20 @@ BOARD_SEPOLICY_DIRS += \
     vendor/ds/sepolicy
 
 BOARD_SEPOLICY_UNION += \
+    file.te \
     file_contexts \
+    dumpstate.te \
+    hostapd.te \
+    init.te \
+    healthd.te \
+    netd.te \
     property.te \
     property_contexts \
+    sdcardd.te \
+    system.te \
+    shell.te \
     sysinit.te \
+    system_app.te \
+    ueventd.te \
     userinit.te \
     mac_permissions.xml
